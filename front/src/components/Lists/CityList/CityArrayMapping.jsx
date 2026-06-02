c47f83573508bf382e07ecdd8eee7e71a06f28f9
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-06-02 10:06:41 +0300

    add img for delete button

--- front/src/components/Lists/CityList/CityArrayMapping.jsx
+++ front/src/components/Lists/CityList/CityArrayMapping.jsx
@@ -1,3 +1,5 @@
+import x from "../../../assets/x.png"
+
 function CityArrayMapping({ cities }) {
 
     return (
@@ -7,7 +9,7 @@ function CityArrayMapping({ cities }) {
 
                 <div className="flex gap-5">
                     <button className="block">update</button>
-                    <button className="block">Delete</button>
+                    <button className="block cursor-pointer"><img src={x} alt="x" className="h-5" /></button>
                 </div>
             </div>
         </>
