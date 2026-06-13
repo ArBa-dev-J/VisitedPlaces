d80718c5f302327b0252242f190f1e0f96dc8c5c
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-06-13 09:40:12 +0300

    add button to show or hide description field

--- front/src/components/Lists/PlacesList/PlacesMapping.jsx
+++ front/src/components/Lists/PlacesList/PlacesMapping.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function PlacesMapping() {
+    const [show, setShow] = useState(null);
+
+    const toShow = () => {
+        return show ? setShow(null) : setShow(true); 
+    }
+
     return (
         <>
             <div className="flex flex-col items-center mx-auto mt-3 border border-white rounded-[25px] w-[90%]">
@@ -13,9 +21,11 @@ function PlacesMapping() {
                     <img className="size-[100%] border rounded-[20px] border-white" src="https://catlintucker.com/wp-content/uploads/2015/02/Catlin-Tucker-Meme-300x198.png" alt="test" />
                 </figure>
 
-                <div style={{width: "90%" }} className="m-5 text-white text-center bg-sky-600 border rounded-[25px]">
+                <button onClick={() => toShow()} className="mb-5 cursor-pointer p-1 rounded-[20px] hover:bg-gray-200 bg-white">Read Description</button>
+
+                {show ? <div className="m-5  text-white text-center bg-sky-600 border rounded-[25px]">
                     <p className="m-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quas illum omnis tempora possimus. Perspiciatis iste quisquam quis unde autem corporis cum fugit tempore hic ipsam, soluta quod ex debitis?</p>
-                </div>
+                </div> : null}
 
             </div>
         </>
