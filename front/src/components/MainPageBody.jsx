c7680c20e2baca621466d4d9963db8cb164d5794
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-06-23 00:07:30 +0300

    add body table for main page

---
+++ front/src/components/MainPageBody.jsx
@@ -0,0 +1,20 @@
+function MainPageBody() {
+    return (
+        <section className="bg-sky-600 px-8 pb-12 mt-9">
+            <div className="mx-auto max-w-6xl">
+                <div className="rounded-3xl bg-white p-8 shadow-lg">
+                    <h2 className="mb-8 text-center text-3xl font-semibold">
+                        Welcome
+                    </h2>
+
+                    <p className="mx-auto mb-10 max-w-3xl text-center text-lg text-gray-600">
+                        Manage your visited places and cities. Add new destinations,
+                        browse your travel history, and organize your adventures.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default MainPageBody;
--- front/src/components/pages/MainPage.jsx
+++ front/src/components/pages/MainPage.jsx
@@ -1,10 +1,13 @@
 import PageHeader from "../PageHeader";
+import MainPageBody from "../MainPageBody";
 
 function MainPage() {
     return (
         <>
             <main className="bg-sky-600">
                 <PageHeader />
+
+                <MainPageBody />
             </main>
         </>
     );
