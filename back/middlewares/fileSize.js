5f93dfb37a82249b03ebb95478f711010b3c73f6
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-06-29 16:39:25 +0300

    add image file size validation

---
+++ back/middlewares/fileSize.js
@@ -0,0 +1,29 @@
+import fs, { unlink } from "fs"
+
+function fileSize(req, res, next) {
+    const maxSize = 30 * 1024 * 1024; // 30 MB
+
+
+    try {
+        if (req.file.size >= maxSize) {
+
+            fs.unlink(req.file.path, (err) => {
+                if (err) console.error(err);
+            });
+
+            return res.status(400).json({
+                status: "fail",
+                message: `The image is too big, 30 MB max`,
+            })
+        }
+
+        next();
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: `${error}`,
+        })
+    }
+}
+
+export default fileSize;
--- back/routes/placesRoutes.js
+++ back/routes/placesRoutes.js
@@ -4,6 +4,7 @@ import validate from "../validation/validation.js";
 import capitalLetter from "../middlewares/capitalLetter.js";
 import paramVal from "../validation/placeSearchParamVal.js";
 import upload from "../middlewares/imageUpload.js";
+import fileSize from "../middlewares/fileSize.js";
 import { newVisitedPlaceC, getAllPlacesC } from "../controllers/placesController.js"; 
 
 const placesRoutes = express.Router();
@@ -11,7 +12,7 @@ const placesRoutes = express.Router();
 // places routes
 
 
-placesRoutes.post("/newPlace", upload.single("image"), placeVal, validate, capitalLetter, newVisitedPlaceC);
+placesRoutes.post("/newPlace", upload.single("image"), fileSize, placeVal, validate, capitalLetter, newVisitedPlaceC);
 placesRoutes.get("/placesList", paramVal, validate, getAllPlacesC);
 
 export default placesRoutes;
