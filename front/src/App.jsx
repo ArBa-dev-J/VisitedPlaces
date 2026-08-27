a9a8dc183be1b53a400fba99f3a5ef725d7505d9
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-08-27 13:36:12 +0300

    make loading circle when main page is loading appear at the center

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,31 @@
-import { Suspense, lazy } from 'react';
-import Loading from './utils/Loading';
+import { Suspense, lazy } from "react";
+import Loading from "./utils/Loading";
 const MainPage = lazy(() => import("./components/pages/MainPage"));
 import NewCityForm from "./components/forms/newCity/NewCityForm";
 import NewPlaceForm from "./components/forms/newPlace/NewPlaceForm";
 import CityListPage from "./components/Lists/CityList/cityListPage";
-import PlacesListPage from "./components/Lists/PlacesList/PlacesListPage"
+import PlacesListPage from "./components/Lists/PlacesList/PlacesListPage";
 import { Routes, Route } from "react-router";
 
-
 function App() {
-
   return (
     <>
       <Routes>
         {/* Dasboard route */}
-        <Route path="/" element={<Suspense fallback={<Loading />}><MainPage /></Suspense>} />
+        <Route
+          path="/"
+          element={
+            <Suspense
+              fallback={
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                  <Loading />
+                </div>
+              }
+            >
+              <MainPage />
+            </Suspense>
+          }
+        />
 
         {/* Form routes */}
         <Route path="/newCity" element={<NewCityForm />} />
@@ -23,11 +34,9 @@ function App() {
         {/* List routes */}
         <Route path="/cityList" element={<CityListPage />} />
         <Route path="/placesList" element={<PlacesListPage />} />
-
       </Routes>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
