ecacce95b714bf987013f99e2dca0fe3ceffd212
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-08-27 13:45:59 +0300

    add lazy import and suspense for the rest

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,56 +1,103 @@
 import { Suspense, lazy } from "react";
 import Loading from "./utils/Loading";
+import { Routes, Route } from "react-router";
+
+// Lazy-loaded components
 const MainPage = lazy(() => import("./components/pages/MainPage"));
 const NewCityForm = lazy(
   () => import("./components/forms/newCity/NewCityForm"),
 );
-import NewPlaceForm from "./components/forms/newPlace/NewPlaceForm";
-import CityListPage from "./components/Lists/CityList/cityListPage";
-import PlacesListPage from "./components/Lists/PlacesList/PlacesListPage";
-import { Routes, Route } from "react-router";
+const NewPlaceForm = lazy(
+  () => import("./components/forms/newPlace/NewPlaceForm"),
+);
+const CityListPage = lazy(
+  () => import("./components/Lists/CityList/cityListPage"),
+);
+const PlacesListPage = lazy(
+  () => import("./components/Lists/PlacesList/PlacesListPage"),
+);
 
 function App() {
   return (
-    <>
-      <Routes>
-        {/* Dasboard route */}
-        <Route
-          path="/"
-          element={
-            <Suspense
-              fallback={
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                  <Loading />
-                </div>
-              }
-            >
-              <MainPage />
-            </Suspense>
-          }
-        />
+    <Routes>
+      {/* Dashboard route */}
+      <Route
+        path="/"
+        element={
+          <Suspense
+            fallback={
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <Loading />
+              </div>
+            }
+          >
+            <MainPage />
+          </Suspense>
+        }
+      />
 
-        {/* Form routes */}
-        <Route
-          path="/newCity"
-          element={
-            <Suspense
-              fallback={
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                  <Loading />
-                </div>
-              }
-            >
-              <NewCityForm />
-            </Suspense>
-          }
-        />
-        <Route path="/newPlace" element={<NewPlaceForm />} />
+      {/* Form routes */}
+      <Route
+        path="/newCity"
+        element={
+          <Suspense
+            fallback={
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <Loading />
+              </div>
+            }
+          >
+            <NewCityForm />
+          </Suspense>
+        }
+      />
 
-        {/* List routes */}
-        <Route path="/cityList" element={<CityListPage />} />
-        <Route path="/placesList" element={<PlacesListPage />} />
-      </Routes>
-    </>
+      <Route
+        path="/newPlace"
+        element={
+          <Suspense
+            fallback={
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <Loading />
+              </div>
+            }
+          >
+            <NewPlaceForm />
+          </Suspense>
+        }
+      />
+
+      {/* List routes */}
+      <Route
+        path="/cityList"
+        element={
+          <Suspense
+            fallback={
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <Loading />
+              </div>
+            }
+          >
+            <CityListPage />
+          </Suspense>
+        }
+      />
+
+      <Route
+        path="/placesList"
+        element={
+          <Suspense
+            fallback={
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <Loading />
+              </div>
+            }
+          >
+            <PlacesListPage />
+          </Suspense>
+        }
+      />
+    </Routes>
   );
 }
 
