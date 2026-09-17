da4b82093da1459d4682c4dd68485725a3efdf08
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-09-17 11:42:31 +0300

    add design and media query for Quick Links table

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,38 +1,44 @@
 function Footer() {
   return (
     <>
-      <footer>
-        <h3 className="text-center">Company Name</h3>
-        <p className="text-center">
-          Providing quality services and solutions for everyone.
-        </p>
-
-        <div className="flex justify-center gap-57">
-          <h4>Quick Links</h4>
-          <ol className="list-disc">
-            <li>
-              <a href="index.html">Home</a>
-            </li>
-            <li>
-              <a href="about.html">About Us</a>
-            </li>
-            <li>
-              <a href="services.html">Services</a>
-            </li>
-            <li>
-              <a href="contact.html">Contact</a>
-            </li>
-          </ol>
+      <footer className="px-8 pb-12 mt-9">
+        <div>
+          <h3 className="text-center">Company Name</h3>
+          <p className="text-center">
+            Providing quality services and solutions for everyone.
+          </p>
         </div>
 
-        <h4 className="text-center">Contact Information</h4>
-        <p className="text-center">Email: info@example.com</p>
-        <p className="text-center">Phone: +1 234 567 890</p>
-        <p className="text-center">Address: 123 Main Street, City, Country</p>
-        <h4 className="text-center">Follow Us</h4>
+        <div className="mx-auto max-w-6xl">
+          <div className="flex justify-center gap-57 2xl:gap-180 rounded-3xl bg-white shadow-lg pt-2 pb-2">
+            <h4 className="font-bold text-[1.2rem]">Quick Links</h4>
+            <ol className="list-disc">
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="about.html">About Us</a>
+              </li>
+              <li>
+                <a href="services.html">Services</a>
+              </li>
+              <li>
+                <a href="contact.html">Contact</a>
+              </li>
+            </ol>
+          </div>
+        </div>
+
+        <div>
+          <h4 className="text-center">Contact Information</h4>
+          <p className="text-center">Email: info@example.com</p>
+          <p className="text-center">Phone: +1 234 567 890</p>
+          <p className="text-center">Address: 123 Main Street, City, Country</p>
+          <h4 className="text-center">Follow Us</h4>
+        </div>
 
         <div className="flex justify-center gap-11">
-          <a href="#">Facebook</a> <a href="#">Instagram</a> 
+          <a href="#">Facebook</a> <a href="#">Instagram</a>
           <a href="#">Twitter</a>
         </div>
       </footer>
