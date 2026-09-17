3354cd2c128653239db6d3cb3317f466c77b163f
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-09-17 12:28:15 +0300

    finish footer design

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 function Footer() {
   return (
     <>
+      <hr />
+
       <footer className="px-8 pb-12 mt-9">
         <div>
-          <h3 className="text-center">Company Name</h3>
-          <p className="text-center">
+          <h3 className="text-center font-bold text-[1.2rem]">
+            Visited Places
+          </h3>
+          <p className="text-center pb-2">
             Providing quality services and solutions for everyone.
           </p>
         </div>
@@ -14,32 +18,36 @@ function Footer() {
             <h4 className="font-bold text-[1.2rem]">Quick Links</h4>
             <ol className="list-disc">
               <li>
-                <a href="index.html">Home</a>
+                <a href="">Home</a>
               </li>
               <li>
-                <a href="about.html">About Us</a>
+                <a href="">About Us</a>
               </li>
               <li>
-                <a href="services.html">Services</a>
+                <a href="">Services</a>
               </li>
               <li>
-                <a href="contact.html">Contact</a>
+                <a href="">Contact</a>
               </li>
             </ol>
           </div>
         </div>
 
-        <div>
-          <h4 className="text-center">Contact Information</h4>
+        <div className="pt-2 pb-2">
+          <h4 className="text-center font-bold text-[1.2rem]">
+            Contact Information
+          </h4>
           <p className="text-center">Email: info@example.com</p>
           <p className="text-center">Phone: +1 234 567 890</p>
           <p className="text-center">Address: 123 Main Street, City, Country</p>
-          <h4 className="text-center">Follow Us</h4>
         </div>
-
-        <div className="flex justify-center gap-11">
-          <a href="#">Facebook</a> <a href="#">Instagram</a>
-          <a href="#">Twitter</a>
+        
+        <h4 className="text-center pb-2 font-bold text-[1.2rem]">Follow Us</h4>
+        <div className="mx-auto max-w-6xl">
+          <div className="flex justify-center gap-11 2xl:gap-87 pt-2 pb-2 rounded-3xl bg-white shadow-lg">
+            <a href="#">Facebook</a> <a href="#">Instagram</a>
+            <a href="#">Twitter</a>
+          </div>
         </div>
       </footer>
     </>
